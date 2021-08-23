55c022bc6854ec65857abdf82c6d9dc87efd5338
crake <remiaston@gmail.com>
2021-08-22 18:17:27 -0700

    chemi bot

--- bot/main.py
+++ bot/main.py
@@ -3,18 +3,42 @@ import os
 #import pynacl
 #import dnspython
 import server
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+from discord.ext import commands
+from random import randrange
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
+
+TOKEN = os.environ['token']
+QUOTA_CHANNEL = os.environ['quota_channel']
+
+quotas = []
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    # get messages from quota
+    channel = bot.get_channel(int(QUOTA_CHANNEL))
+    global quotas
+    quotas = await channel.history().flatten()
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
+@bot.event
+async def on_message(msg):
+    # add new quotable message if one is posted in quota
+    if msg.channel.id == int(QUOTA_CHANNEL):
+      quotas.append(msg)
+    # post a message from quotas
+    if bot.user.mentioned_in(msg):
+        resp = quotas[randrange(len(quotas))]
+        # send attachments if there is one
+        if len(resp.attachments) > 0:
+          embed = discord.Embed()
+          embed.set_image(url=resp.attachments[0].url)
+          await msg.channel.send(embed=embed, content=resp.content)
+        # send vanilla text message
+        else:
+          await msg.channel.send(content=resp.content)
+      
 server.server()
 bot.run(TOKEN)
+
